81c5468d74886e668acae90b8f9e065d78555030
ThiiD <thiago.saber@engenharia.ufjf.br>
2021-12-14 11:19:04 -0300

    Update identificaParametros.py

--- Python/identificaParametros.py
+++ Python/identificaParametros.py
@@ -9,24 +9,22 @@ class identificaParametros():
     def __init__(self, dados):
         data = pd.read_csv(f'Python\\Dados\\{dados}.csv', sep=';')
 
-        valor_inicial = sum(data['Velocidade'][:5000])/len(data['Velocidade'][:5000])
-        valor_final = sum(data['Velocidade'][8000:])/len(data['Velocidade'][8000:])
+        valor_inicial = sum(data['Velocidade'][:int(len(data['Velocidade'])/2)])/len(data['Velocidade'][:int(len(data['Velocidade'])/2)])
+        valor_final = sum(data['Velocidade'][int(4*len(data['Velocidade'])/5):])/len(data['Velocidade'][int(4*len(data['Velocidade'])/5):])
         print(f'Valor final: {valor_final}')
 
-        valor_tau = valor_inicial + ((valor_final - valor_inicial)*0.632)
-        indice_tau = 0
         for i in range(len(data['Velocidade'])):
-            # if data[len(data['Velocidade']) - 1 - i] > valor_final*1.02 or data[len(data['Velocidade']) - 1 - i] < valor_final*0.98:
-            #     indice_tau = len(data['Velocidade']) - i
-            #     break
-
             if data['Velocidade'][len(data['Velocidade']) - i - 1] > valor_final*1.02 or data['Velocidade'][len(data['Velocidade']) - i - 1] < valor_final*0.98:
                 print(len(data['Velocidade']) - i - 1)
-                indice_tau = len(data['Velocidade']) - 1 - i
-                tau = (indice_tau/5000) - 5
+                tempo_acomodacao = len(data['Velocidade']) - i - 1   # Tempo de acomodação para 2%
+                print(f'tempo_acomodacao: {tempo_acomodacao}')
+                tau = (tempo_acomodacao/1000 - 5)/4
                 break
 
-        print(indice_tau) 
+        print(tau) 
+
+        sigma = 1/tau
+
 
 
 
